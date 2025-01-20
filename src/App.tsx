50b6baa25ebcdbca36d572853db48956f4d1fc07
MaxLyovin <maxim.lyovin@tsh.io>
2025-01-20 16:30:42 +0100

    feat: config shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import "./App.css";
 
 import { CreateTemplate } from "./pdf/wniosekZC/CreateTemplate/CreateTemplate";
 import { ReadAndFillPdf } from "./ReadAndFill/ReadAndFillPdf";
+import { Button } from "./components/ui/button";
 
 function App() {
   return (
     <div>
-      <ReadAndFillPdf />
-      <CreateTemplate />
+      {/* <ReadAndFillPdf />
+      <CreateTemplate /> */}
+      <Button variant={"destructive"} onClick={() => console.log("tadat")}>
+        button
+      </Button>
     </div>
   );
 }
