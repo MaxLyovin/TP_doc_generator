92302895d1b235da1b6894654c24d980984743ad
Maxim Lyovin <lyovin.maxim@gmail.com>
2025-02-13 18:41:29 +0100

    feat: travels outside poland

---
+++ src/UserDataForm/components/SteppedContent/components/PreviousVisits/components/TravelsOutsidePoland/hooks/useTravelsOutsidePoland.tsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import { Travel } from "../TravelsOutsidePoland";
+import { useUserData } from "@/state/hooks/useUserData";
+
+export const useTravelsOutsidePoland = () => {
+  const { userData } = useUserData();
+  const [travels, setTravels] = useState<Travel[]>(
+    userData?.travelsOutsidePoland || []
+  );
+
+  const addTravel = (travel: Travel) =>
+    setTravels((travels) => [...travels, travel]);
+
+  const removeTravel = (travelIndex: number) =>
+    setTravels((travels) =>
+      travels.filter((_travel, index) => index !== travelIndex)
+    );
+  return { travels, addTravel, removeTravel };
+};
