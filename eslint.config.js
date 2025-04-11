285ca3da471e6369348e0a5385355f6e4b859407
Maxim Lyovin <lyovin.maxim@gmail.com>
2025-04-11 17:46:47 +0200

    feat: add prettier, add import eslint plugin, config eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,21 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
+import importPlugin from 'eslint-plugin-import';
 
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      eslintConfigPrettier,
+      importPlugin.flatConfigs.recommended,
+    ],
+    files: ['**/*.{ts,tsx,js,jsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
@@ -17,12 +24,39 @@ export default tseslint.config(
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
+    settings: {
+      'import/parsers': {
+        '@typescript-eslint/parser': ['.ts', '.tsx', '.js', '.jsx'],
+      },
+      'import/resolver': {
+        typescript: {
+          alwaysTryTypes: true,
+          project: './tsconfig.json',
+        },
+        alias: {
+          map: [['@', './src']],
+        },
+      },
+    },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      '@typescript-eslint/no-unused-vars': ['warn'],
+      'import/no-cycle': ['off'],
+      'import/namespace': ['off'],
+      'import/no-named-as-default': ['off'],
+      'import/default': ['warn'],
+      'import/order': [
+        'error',
+        {
+          groups: [
+            ['external', 'builtin'],
+            ['parent', 'internal'],
+            ['index', 'sibling'],
+          ],
+          'newlines-between': 'always',
+        },
       ],
     },
   },
-)
+);
