ac78d3ae27e847f0925f3caf3b79b12e959f0b7c
Maxim Lyovin <lyovin.maxim@gmail.com>
2025-05-05 16:16:41 +0200

    feat: install and config react-pdf

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,18 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { pdfjs } from 'react-pdf';
+import './index.css';
+import 'react-pdf/dist/Page/TextLayer.css';
+import 'react-pdf/dist/Page/AnnotationLayer.css';
 
-import "./index.css";
-import App from "./App.tsx";
-import "./i18n/i18n.ts";
+import App from './App.tsx';
 
-createRoot(document.getElementById("root")!).render(
+import './i18n/i18n.ts';
+
+pdfjs.GlobalWorkerOptions.workerSrc = new URL('pdfjs-dist/build/pdf.worker.min.mjs', import.meta.url).toString();
+
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-  </StrictMode>
+  </StrictMode>,
 );
