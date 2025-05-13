85910c5706b60dead572e8231ecd75a66e239b44
artemios999 <140052996+artemios999@users.noreply.github.com>
2025-05-13 20:10:02 +0200

    Styles in mobile view

--- src/UserDataForm/components/SteppedContent/components/PreviousVisits/components/PreviousPolandVisits/PreviousPolandVisits.tsx
+++ src/UserDataForm/components/SteppedContent/components/PreviousVisits/components/PreviousPolandVisits/PreviousPolandVisits.tsx
@@ -1,14 +1,13 @@
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useTranslation } from 'react-i18next';
 
-import { useForm } from "react-hook-form";
-import { z } from "zod";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useTranslation } from "react-i18next";
+import { Form } from '@/components/ui/form';
+import { InputField } from '@/components/form';
+import { Button } from '@/components/ui/button';
 
-import { Form } from "@/components/ui/form";
-import { InputField } from "@/components/form";
-import { Button } from "@/components/ui/button";
-
-import { PolandVisitsTable } from "./PolandVisitsTable";
+import { PolandVisitsTable } from './PolandVisitsTable';
 
 const formSchema = z.object({
   from: z.string(),
@@ -28,11 +27,7 @@ type PreviousPolandVisitsProps = {
   removeVisit: (visitIndex: number) => void;
 };
 
-export const PreviousPolandVisits = ({
-  visits,
-  addVisit,
-  removeVisit,
-}: PreviousPolandVisitsProps) => {
+export const PreviousPolandVisits = ({ visits, addVisit, removeVisit }: PreviousPolandVisitsProps) => {
   const { t } = useTranslation();
 
   const form = useForm<z.infer<typeof formSchema>>({
@@ -51,44 +46,37 @@ export const PreviousPolandVisits = ({
   return (
     <div>
       <div>
-        <h3 className="font-semibold mb-4">
-          {t("common.previous_poland_stay")}
-        </h3>
+        <h3 className="font-semibold mb-4">{t('common.previous_poland_stay')}</h3>
         <div className="mb-6">
           <Form {...form}>
-            <form
-              onSubmit={form.handleSubmit(onSubmit)}
-              className="space-y-4 text-start"
-            >
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4 text-start">
               <InputField
-                controllerProps={{ control: form.control, name: "legalBase" }}
-                label={t(
-                  "main_form.field.previous_poland_stay_legalBase.label"
-                )}
+                controllerProps={{ control: form.control, name: 'legalBase' }}
+                label={t('main_form.field.previous_poland_stay_legalBase.label')}
               />
-              <div className="flex gap-4 items-end justify-between">
+              <div className="flex flex-col gap-4 items-stretch justify-between sm:flex-row sm:items-end">
                 <InputField
-                  controllerProps={{ control: form.control, name: "from" }}
-                  label={t("main_form.field.previous_poland_stay_from.label")}
-                  inputProps={{ type: "date" }}
+                  controllerProps={{ control: form.control, name: 'from' }}
+                  label={t('main_form.field.previous_poland_stay_from.label')}
+                  inputProps={{ type: 'date' }}
+                  className="w-full"
                 />
                 <InputField
-                  controllerProps={{ control: form.control, name: "to" }}
-                  label={t("main_form.field.previous_poland_stay_to.label")}
-                  inputProps={{ type: "date" }}
+                  controllerProps={{ control: form.control, name: 'to' }}
+                  label={t('main_form.field.previous_poland_stay_to.label')}
+                  inputProps={{ type: 'date' }}
+                  className="w-full"
                 />
 
-                <Button className="grow" type="submit">
-                  {t("common.add")}
+                <Button className="w-full sm:grow" type="submit">
+                  {t('common.add')}
                 </Button>
               </div>
             </form>
           </Form>
         </div>
       </div>
-      {!!visits.length && (
-        <PolandVisitsTable visits={visits} removeVisit={removeVisit} />
-      )}
+      {!!visits.length && <PolandVisitsTable visits={visits} removeVisit={removeVisit} />}
     </div>
   );
 };
